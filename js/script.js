5a9f61b52017ecb6d4cc2efffaeee611535af2f8
Katya Prymak <62486148+katyaprymak@users.noreply.github.com>
2024-01-07 15:22:07 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var footerColor = document.querySelector("footer");
+footerColor.style.backgroundColor = "#d7ddf3";
+
+var editText = document.querySelector(".tagline");
+editText.innerHTML = "express yourself";
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.75em";
+
+var boldText = document.querySelector("h3");
+boldText.innerHTML = "We're here for you <em>every day</em> of the week.";
